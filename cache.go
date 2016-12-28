d3dc938d7b685622ff12edc1bd8c2075b5bff693
esp <esp@liferoot.ru>
2016-12-29 00:42:39 +0300

    Impl. ShadeCallback

--- cache.go
+++ cache.go
@@ -7,7 +7,7 @@ type Cache interface {
 	Len() int
 	Clear()
 	Contains(key interface{}) bool
-	Each(n int, f EachCallback)
+	Each(n int, each EachCallback)
 	Get(key interface{}) (value interface{}, err error)
 	Peek(key interface{}) (value interface{}, ok bool)
 	Put(key, value interface{})
@@ -17,6 +17,7 @@ type Cache interface {
 type EachCallback func(key, value interface{})
 type EvictCallback func(key, value interface{})
 type OmitCallback func(key interface{}) (value interface{}, ok bool)
+type ShadeCallback func(key, value interface{})
 
 var (
 	ErrKeyNotFound = errors.New(`key not found`)
